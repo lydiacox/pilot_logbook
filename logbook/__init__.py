385d77bc9e2f6d658da9ac27502cb41a53834613
lydiacox <coxlydia01@gmail.com>
2021-10-29 11:30:02 +1100

    add more files, create Pilot class

--- logbook/__init__.py
+++ logbook/__init__.py
@@ -1,6 +1,41 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+(
+    db_user,
+    db_pass,
+    db_name,
+    db_domain
+) = (os.environ.get(item) for item in [
+    "DB_USER",
+    "DB_PASS",
+    "DB_NAME",
+    "DB_DOMAIN"
+    ]
+)
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgres+psycopg2://{db_user}:{db_pass}@{db_domain}/{db_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+
+db = SQLAlchemy(app)
+
+class Pilot(db.Model):
+    __tablename__ = "pilots"
+    pilot_id = db.Column(db.Integer, primary_key=True)
+    pilot_name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, pilot_id):
+        self.pilot_id = pilot_id
+
+    @property
+    def serialize(self):
+        return {
+            "pilot_id": self.pilot_id,
+            "pilot_name": self.pilot_name
+        }
 
 @app.route('/')
 def hello_world():
