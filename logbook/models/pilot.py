fb76a2c1aaa54aa515074beca0bab88ecf1c3f0f
lydiacox <coxlydia01@gmail.com>
2021-11-13 16:28:14 +1100

    modify tests

--- logbook/models/pilot.py
+++
@@ -1,9 +0,0 @@
-from main import db
-
-class Pilot(db.Model):
-        __tablename__ = "pilots"
-        pilot_id = db.Column(db.Integer, primary_key=True)
-        pilot_name = db.Column(db.String(100), nullable=False)
-
-        def __init__(self, pilot_name):
-            self.pilot_name = pilot_name
--- logbook/tests/test_pilots.py
+++ logbook/tests/test_pilots.py
@@ -7,38 +7,38 @@ load_dotenv()
 
 os.environ["FLASK_ENV"] = "testing"
 
-class TestPilots(unittest.TestCase):
+class TestFlights(unittest.TestCase):
     def setUp(self):
         self.app = create_app()
         self.client = self.app.test_client()
 
-    def test_pilot_index(self):
-        response = self.client.get("/pilots/")
+    def test_flight_index(self):
+        response = self.client.get("/flights/")
         data = response.get_json()
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'<h1>Pilot Index</h1>', response.data)
+        self.assertIn(b'<h1>Flight Index</h1>', response.data)
 
-    def test_create_bad_pilot(self):
-        response = self.client.post("/pilots/", data={"pilot_name": ""})
+    def test_create_bad_flight(self):
+        response = self.client.post("/flights/", data={"flight_no": ""})
         self.assertEqual(response.status_code, 400)
 
-    def test_create_good_pilots(self):
-        response = self.client.post("/pilots/", data={"pilot_name": "testpilot"})
+    def test_create_good_flights(self):
+        response = self.client.post("/flights/", data={"flight_no": "testflight"})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json()["pilot_name"], "testpilot")
-        self.client.delete(f"/pilots/{response.get_json()['pilot_id']}/")
+        self.assertEqual(response.get_json()["flight_no"], "testflight")
+        self.client.delete(f"/flights/{response.get_json()['flight_id']}/")
     
-    def test_delete_pilot(self):
-        response1 = self.client.post("/pilots/", data={"pilot_name": "testpilot"})
-        id = response1.get_json()["pilot_id"]
-        response2 = self.client.delete(f"/pilots/{id}/")
+    def test_delete_flight(self):
+        response1 = self.client.post("/flights/", data={"flight_no": "testflight"})
+        id = response1.get_json()["flight_id"]
+        response2 = self.client.delete(f"/flights/{id}/")
         self.assertEqual(response2.status_code, 200)
 
-    def test_update_pilot(self):
-        response1 = self.client.post("/pilots/", data={"pilot_name": "testpilot"})
-        id = response1.get_json()["pilot_id"]
-        response2 = self.client.put(f"/pilots/{id}/", json={"pilot_name": "newtestpilot"})
+    def test_update_flight(self):
+        response1 = self.client.post("/flights/", data={"flight_no": "testflight"})
+        id = response1.get_json()["flight_id"]
+        response2 = self.client.put(f"/flights/{id}/", json={"flight_no": "newtestflight"})
         self.assertEqual(response2.status_code, 200)
-        self.assertEqual(response2.get_json()["pilot_name"], "newtestpilot")
-        self.client.delete(f"/pilots/{id}/")
+        self.assertEqual(response2.get_json()["flight_no"], "newtestflight")
+        self.client.delete(f"/flights/{id}/")
