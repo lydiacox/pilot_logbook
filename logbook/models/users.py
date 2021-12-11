385baf764594c4835940562d288ec86b7b6d67cb
lydiacox <coxlydia01@gmail.com>
2021-12-11 17:00:35 +1100

    add user pilot relationship

--- logbook/models/users.py
+++ logbook/models/users.py
@@ -77,6 +77,11 @@ class User(UserMixin, db.Model):
         backref="creator",
         lazy="joined"
     )
+    pilot = db.relationship(
+        "Pilot",
+        backref="user",
+        lazy="joined"
+    )
     # To access the list of flights created by a user, we call
     # Username.flights = [<Flight 1>, <Flight 2>]
     # To access the creator of a flight, we call Flightno.creator
