d3248b47399bea3e1059d738769006d8fa8f2d8d
lydiacox <coxlydia01@gmail.com>
2021-12-13 18:07:19 +1100

    update approach schema

--- logbook/schemas/approach_schema.py
+++ logbook/schemas/approach_schema.py
@@ -1,1 +1,17 @@
-from main import Marshmallow
+from main import ma
+from models.approach import InstrumentApproach
+from marshmallow_sqlalchemy import auto_field
+
+class ApproachSchema(ma.SQLAlchemyAutoSchema):
+    """
+    A class to represent an instrument approach schema.
+    ...
+    Attributes
+    ----------
+    """
+
+ifr_approach_id = auto_field(dump_only=True)
+# add more validation??
+approach_type = auto_field(required=True)
+approach_flight_id = auto_field(required=True)
+approach_flight = auto_field(required=True)
